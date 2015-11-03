7b11d0bbec5ae29296884545d44a198865b5dc66
hgv27681 <hgv27681@e099a375-04f9-0310-9d5f-a741eaff62e1>
2015-11-03 16:14:17 +0000

    update to new dls-environment for module.ini support
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/dls_dependency_tree@138765 e099a375-04f9-0310-9d5f-a741eaff62e1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     version = version,
     packages = ["dls_dependency_tree"],
     # define console_scripts to be 
-    install_requires = [ "dls_environment == 4.8" ],
+    install_requires = [ "dls_environment == 4.9" ],
     entry_points = {'console_scripts': \
                     ['dls-dependency-checker.py = dls_dependency_tree.dependency_checker:dependency_checker']},
 #    include_package_data = True,
