f5bb57e10fcb9b81bff3a48abbfb4c015cf7fd0d
Dimitrios Ladakis <dimitrios.ladakis@diamond.ac.uk>
2019-04-24 09:48:49 +0100

    port to python3

--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
 from setuptools import setup
 
-# these lines allow the version to be specified in Makefile.private
-import os
-version = os.environ.get("MODULEVER", "0.0")
 
-setup(
-    # name of the module
-    name = "dls_dependency_tree",
-    # version: over-ridden by the release script
-    version = version,
-    packages = ["dls_dependency_tree"],
-    # define console_scripts to be 
-    install_requires = [ "dls_ade>=0.5" ],
-    entry_points = {'console_scripts': \
-                    ['dls-dependency-checker.py = dls_dependency_tree.dependency_checker:dependency_checker']},
-#    include_package_data = True,
-    zip_safe = False
-    )
+setup()
