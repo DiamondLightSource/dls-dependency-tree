b4389f914ff0e0be69d23e8743bedba5b686aa4b
Emma Dixon <emma.dixon@diamond.ac.uk>
2020-04-20 13:50:01 +0100

    Upgrade PyQt4 to PyQt5
    - QtGui splits into QtGui and QtWidget
    - SIGNALs updated
    - QStringList deprecated
    - Use uic to load ui file

--- dls_dependency_tree/dependency_checker.py
+++ dls_dependency_tree/dependency_checker.py
@@ -53,30 +53,30 @@ def build_gui_tree(list_view,tree,parent=None):
     if parent==None:
         list_view.clear()
     if parent:
-        child = QtGui.QTreeWidgetItem(parent)
+        child = QTreeWidgetItem(parent)
     else:
-        child = QtGui.QTreeWidgetItem(list_view)
+        child = QTreeWidgetItem(list_view)
         list_view.child = child
     child.setText(0, "%s: %s" % (tree.name, tree.version))
     child.tree = tree
-    fg = QtGui.QBrush(QtCore.Qt.black)
-    bg = QtGui.QBrush(QtGui.QColor(212,216,236)) # normal - blue
+    fg = QBrush(Qt.black)
+    bg = QBrush(QColor(212,216,236)) # normal - blue
     open_parents = False
     if len(tree.updates())>1:
-        bg = QtGui.QBrush(QtGui.QColor(203,255,197)) # update available - green
+        bg = QBrush(QColor(203,255,197)) # update available - green
         open_parents = True
     if tree.name in list(list_view.clashes.keys()):
         open_parents = True
         if tree.path == tree.e.sortReleases([x.path for x in \
                                              list_view.clashes[tree.name]])[-1]:
-            fg = QtGui.QBrush(QtGui.QColor(153,150,0)) # involved in clash: yellow
+            fg = QBrush(QColor(153,150,0)) # involved in clash: yellow
         else:
-            fg = QtGui.QBrush(QtCore.Qt.red) # causes clash: red
+            fg = QBrush(Qt.red) # causes clash: red
     if tree.version == "invalid":
         open_parents = True
-        fg = QtGui.QBrush(QtGui.QColor(160,32,240)) # invalid: purple
-    child.setForeground(0, fg)        
-    child.setBackground(0, bg)        
+        fg = QBrush(QColor(160,32,240)) # invalid: purple
+    child.setForeground(0, fg)
+    child.setBackground(0, bg)
     if open_parents: 
         temp_ob = child
         while temp_ob.parent():
@@ -87,16 +87,16 @@ def build_gui_tree(list_view,tree,parent=None):
     if parent==None:
         child.setExpanded(True)
 
-class TreeView(QtGui.QTreeWidget):
+class TreeView(QTreeWidget):
     """Custom tree view widget"""
     def __init__(self,tree,tree_type,*args):
         """Initialise the class.
         tree = dependency_tree to initialise from
         tree_type = string "original","consistent" or "latest" """
-        QtGui.QTreeWidget.__init__(self,*args)
+        QTreeWidget.__init__(self,*args)
         self.setHeaderLabel("%s Tree"%(tree_type.title()))
         palette = self.viewport().palette()
-        palette.setColor(QtGui.QPalette.Base, QtGui.QColor(212,216,236))
+        palette.setColor(QPalette.Base, QColor(212,216,236))
         self.viewport().setPalette(palette)
         self.tree = tree
         self.clashes = tree.clashes(print_warnings=False)
@@ -114,7 +114,7 @@ class TreeView(QtGui.QTreeWidget):
         pos = event.globalPos()
         item = self.itemAt(event.pos())
         if item:
-            menu = QtGui.QMenu()
+            menu = QMenu()
             self.contextItem = item
             menu.addAction("Edit RELEASE", self.externalEdit)
             if hasattr(item.tree,"versions"):
@@ -140,20 +140,20 @@ class TreeView(QtGui.QTreeWidget):
         text = stdout.strip()
         x = formLog(text,self)
         x.setWindowTitle("SVN Log: %s"%leaf.name)
-        x.show()        
+        x.show()
 
     def externalEdit(self):
         """Open the configure/RELEASE in gedit"""
         item = self.contextItem
         if item and os.path.isfile(item.tree.release()):
-            proc = QtCore.QProcess(self)            
-            proc.start("gedit",QtCore.QStringList(item.tree.release()))
-                                        
+            proc = QProcess(self)
+            proc.start("gedit",[item.tree.release()])
+
     def mouseout(self):
         """Show hints in the statusBar on mouseout"""
         self.top.statusBar.showMessage("----- Hover over a module for its path, "\
                                      "right click for a context menu -----")
-                
+
     def mousein(self, item, col):
         """Show item path in the statusBar on mousein"""
         text = "%s - current: %s" %(item.tree.name, item.tree.path)
@@ -164,17 +164,17 @@ class TreeView(QtGui.QTreeWidget):
 
     def confirmWrite(self):
         """Popup a confimation box for writing changes to RELEASE"""
-        response=QtGui.QMessageBox.question(None,"Write Changes",\
+        response=QMessageBox.question(None,"Write Changes",\
              "Would you like to write your changes to configure/RELEASE?",\
-             QtGui.QMessageBox.Yes,QtGui.QMessageBox.No)
-        if response == QtGui.QMessageBox.Yes:
+             QMessageBox.Yes,QMessageBox.No)
+        if response == QMessageBox.Yes:
             self.update.write_changes()
 
     def printChanges(self):
         text = self.update.print_changes()
         x = formLog(text,self)
         x.setWindowTitle("RELEASE Changes")
-        x.show() 
+        x.show()
 
 class reverter:
     """One shot class to revert a leaf in a list view to path"""
@@ -185,7 +185,7 @@ class reverter:
         self.leaf = leaf
         self.list_view = list_view
         self.path = path
-        
+
     def revert(self):
         """Do the revert"""
         new_leaf = dependency_tree(self.leaf.parent,self.path)
@@ -194,22 +194,22 @@ class reverter:
         self.list_view.clashes=self.list_view.tree.clashes(print_warnings=False)
         build_gui_tree(self.list_view,self.list_view.tree)
 
-class formLog(QtGui.QDialog):
+class formLog(QDialog):
     """SVN log form"""
     def __init__(self,text,*args):
         """text = text to display in a readonly QTextEdit"""
-        QtGui.QDialog.__init__(self,*args)
-        formLayout = QtGui.QGridLayout(self)#,1,1,11,6,"formLayout")
-        self.scroll = QtGui.QScrollArea(self)        
-        self.lab = QtGui.QTextEdit()
-        self.lab.setFont(QtGui.QFont('monospace', 10))
+        QDialog.__init__(self,*args)
+        formLayout = QGridLayout(self)#,1,1,11,6,"formLayout")
+        self.scroll = QScrollArea(self)
+        self.lab = QTextEdit()
+        self.lab.setFont(QFont('monospace', 10))
         self.lab.setText(text)
         self.lab.setReadOnly(True)
         self.scroll.setWidget(self.lab)
         self.scroll.setWidgetResizable(True)
-        self.scroll.setMinimumWidth(700)        
-        formLayout.addWidget(self.scroll,1,1)        
-        self.btnClose = QtGui.QPushButton("btnClose", self)
+        self.scroll.setMinimumWidth(700)
+        formLayout.addWidget(self.scroll,1,1)
+        self.btnClose = QPushButton("btnClose", self)
         formLayout.addWidget(self.btnClose,2,1)
         self.btnClose.clicked.connect(self.close)
         self.btnClose.setText("Close")
@@ -221,46 +221,43 @@ def dependency_checker():
     args = parser.parse_args()
     path = os.path.abspath(args.module_path)
 
-    app = QtGui.QApplication([])
-    top = Ui_Form1()
-    window = QtGui.QMainWindow()
-    top.setupUi(window)
+    app = QApplication([])
+    window = QMainWindow()
+    top = uic.loadUi(UI_FILE, window)
     top.statusBar = window.statusBar()
     tree = dependency_tree(None,path)
     window.setWindowTitle("Tree Browser - %s: %s, Epics: %s" % (
                             tree.name, tree.version, tree.e.epicsVer()))
-    
+
     for loc in ["original","latest","consistent"]:
         def displayMessage(message):
             getattr(top,loc+"Write").setEnabled(False)
             getattr(top,loc+"Print").setEnabled(False)
-            label = QtGui.QTextEdit(getattr(top,loc+"Frame"))
+            label = QTextEdit(getattr(top,loc+"Frame"))
             label.setReadOnly(True)
             label.setText(loc.title() + " Updated Tree:\n\n" + message)
             return label
-        grid = QtGui.QGridLayout()            
+        grid = QGridLayout()
         try:
             update = dependency_tree_update(tree,consistent=(loc=="consistent"),update=(loc!="original"))
             if loc=="original" or not update.new_tree == tree:
                 view = TreeView(update.new_tree,loc,getattr(top,loc+"Frame"))
                 view.top = top
                 view.update = update
-                view.connect(getattr(top,loc+"Write"), SIGNAL("clicked()"),\
-                                view.confirmWrite)
-                view.connect(getattr(top,loc+"Print"), SIGNAL("clicked()"),\
-                                view.printChanges)        
-                grid.addWidget(view)                                            
+                getattr(top,loc+"Write").clicked.connect(view.confirmWrite)
+                getattr(top,loc+"Print").clicked.connect(view.printChanges)
+                grid.addWidget(view)
             else:
                 grid.addWidget(displayMessage("Updated tree is identical to Original tree"))
-                
+
         except:
             grid.addWidget(displayMessage("Error in tree update...\n\n"+traceback.format_exc()))
-        getattr(top,loc+"Frame").setLayout(grid)          
-                
+        getattr(top,loc+"Frame").setLayout(grid)
+
     window.show()
     # catch CTRL-C
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     app.exec_()
-                                                        
+
 if __name__ == "__main__":
     dependency_checker()
